ead24dc3048a8b50047f86aafc5c22280659111e
Zachary Ross <zmr462@usask.ca>
2021-10-09 01:46:47 -0600

    feat(mibs): add postgres connection

--- src/projects/mibs/src/app.py
+++ src/projects/mibs/src/app.py
@@ -1,8 +1,10 @@
 from flask import Flask, request
-
+from models import db, Message, EmailMessageRecipient
 
 app = Flask(__name__)
 
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://user:passsword@localhost:5432'
+db.init_app(app)
 
 @app.route('/mibs/hello',methods=['POST','GET'])
 def info():
