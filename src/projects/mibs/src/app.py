c97cebe58032324f5eb4a85f878c323a9a9e0eb3
Zachary Ross <zmr462@usask.ca>
2021-10-12 15:38:38 -0600

    fix: move mibs table creation to SQLAlchemy

--- src/projects/mibs/src/app.py
+++ src/projects/mibs/src/app.py
@@ -8,7 +8,10 @@ db_name = os.environ.get('DB_DATABASE')
 db_user = os.environ.get('DB_USER')
 db_pass = os.environ.get('DB_PASSWORD')
 app.config['SQLALCHEMY_DATABASE_URI'] = f"postgresql+psycopg2://{db_user}:{db_pass}@postgres/{db_name}"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db.init_app(app)
+with app.app_context():
+    db.create_all()
 
 
 @app.route('/mibs/hello',methods=['POST','GET'])
