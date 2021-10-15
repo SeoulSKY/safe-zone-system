06c55b292b9e8e41bdac3e71788f4d2e2ac7c012
Zachary Ross <zmr462@usask.ca>
2021-10-15 11:01:27 -0600

    fix: add import for web project
    the generated api can be imported by both reac-native and web.
    the web project use craco due to issues that occur while importing the
    local module.
    switch the API generator to use typescript-axios instead of typescript
    fetch. This is because of an error `Attempted import error: ... not exported
    from './'` that was occuring while using the typescript-fetch generator.
    
    closes #86

--- src/tools/api/gen_api.sh
+++ src/tools/api/gen_api.sh
@@ -6,6 +6,6 @@ DIR_PATH="$(cd "$(dirname "${BASH_SOURCE[0]}")/../../" && pwd)"
 docker run --rm \
   -v ${DIR_PATH}:/local openapitools/openapi-generator-cli generate \
   -i /local/tools/api/openapi.yml \
-  -g typescript-fetch \
+  -g typescript-axios \
   -o /local/lib/mibs/ts/openapi
 
