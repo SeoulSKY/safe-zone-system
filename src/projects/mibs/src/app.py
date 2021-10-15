5e58a984fd9712ed37b18128a022a47f191491e5
Zachary Ross <zmr462@usask.ca>
2021-10-14 22:08:08 -0600

    fix: pylint errors

--- src/projects/mibs/src/app.py
+++ src/projects/mibs/src/app.py
@@ -10,20 +10,22 @@ app = Flask(__name__)
 db_name = os.environ.get('DB_DATABASE')
 db_user = os.environ.get('DB_USER')
 db_pass = os.environ.get('DB_PASSWORD')
+db_uri = f'postgresql+psycopg2://{db_user}:{db_pass}@postgres/{db_name}'
 
-app.config['SQLALCHEMY_DATABASE_URI'] = f'postgresql+psycopg2://{db_user}:{db_pass}@postgres/{db_name}'
+app.config['SQLALCHEMY_DATABASE_URI'] = db_uri
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db.init_app(app)
 with app.app_context():
-    db.create_all()
+  db.create_all()
 
 
-'''
-Return message for GET request
-'''
+
 @app.route('/mibs/hello',methods=['POST','GET'])
 def info():
+  '''
+  Return message for GET request
+  '''
   if request.method == 'GET':
     return 'Hello from MIBS'
   else:
@@ -32,7 +34,7 @@ def info():
 
 @app.route('/mibs/db_test',methods=['POST','GET'])
 def db_test():
-    if request.method == 'GET':
-        return f"{Message.query.all()}"
-    else:
-        return 'No Get'
+  if request.method == 'GET':
+    return f'{Message.query.all()}'
+  else:
+    return 'No Get'
