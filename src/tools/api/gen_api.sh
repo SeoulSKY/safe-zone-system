0f8054ef6e1e82437e8af164c3f641ed06207684
Zachary Ross <zmr462@usask.ca>
2021-10-17 14:17:54 -0600

    fix: minor issue with TS codegen
    gen_api.sh handles spaces in path names.
    update package-lock.json for web and react-native projects. running npm
    install for these projects will now install dependencies correctly.
    
    note: gen_api.sh and the module imports do not currently work with
    docker.

--- src/tools/api/gen_api.sh
+++ src/tools/api/gen_api.sh
@@ -4,7 +4,7 @@
 DIR_PATH="$(cd "$(dirname "${BASH_SOURCE[0]}")/../../" && pwd)"
 
 docker run --rm \
-  -v ${DIR_PATH}:/local openapitools/openapi-generator-cli generate \
+  -v "${DIR_PATH}":/local openapitools/openapi-generator-cli generate \
   -i /local/tools/api/openapi.yml \
   -g typescript-axios \
   -o /local/lib/mibs/ts/openapi
