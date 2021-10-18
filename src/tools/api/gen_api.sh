254b2dbe6aa76167a03bea29ba8ab01050957242
Zachary Ross <zmr462@usask.ca>
2021-10-17 18:31:12 -0600

    fix: automate ts api generation with docker

--- src/tools/api/gen_api.sh
+++ src/tools/api/gen_api.sh
@@ -1,11 +1,29 @@
+#!/bin/sh -e
 
-#!/bin/bash -e
+# gen_api.sh
+#
+# Generate a Typescript module for a given API.
+#
+# Arguments:
+#   - $1 an openapi document
+#   - $2 the output directory of the API library
+#
+# Pre-conditions:
+#   - node must be installed
+#
+# Post-conditions:
+#   - creates the ts/openapi directories in the given output directory or
+#       the default: /src/tools/api/openapi.yml
+#   - generates a Typescript module in the directory $2/ts/openapi or the
+#       default: /src/lib/mibs/ts
+#
+# Example:
+# generate the mibs api module:
+#   - `$ ./gen_api.sh openapi.yml ../../lib/mibs/ts`
 
-DIR_PATH="$(cd "$(dirname "${BASH_SOURCE[0]}")/../../" && pwd)"
+DIR_PATH="$(dirname "$0")/../../"
 
-docker run --rm \
-  -v "${DIR_PATH}":/local openapitools/openapi-generator-cli generate \
-  -i /local/tools/api/openapi.yml \
-  -g typescript-axios \
-  -o /local/lib/mibs/ts/openapi
+npx @openapitools/openapi-generator-cli generate -g typescript-axios \
+  -i "${1:-$DIR_PATH/tools/api/openapi.yml}" \
+  -o "${2:-$DIR_PATH/lib/mibs/ts}"/openapi
 
