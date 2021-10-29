2e392cfee50c33a0f6915601e85f7b45642dda3c
Jorgen Wiebe <jorgenwiebe@gmail.com>
2021-10-29 17:44:00 -0600

    Feat: Create way to run python tests through docker

--- src/projects/mibs/entrypoint.sh
+++ src/projects/mibs/entrypoint.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 
 export PYTHONPATH=$PYTHONPATH:/usr/local/lib/python3.8/site-packages:/usr/lib/python3.8/site-packages:/app:/app/lib/mibs/python/openapi
-uwsgi --ini /app/uwsgi.ini &
-source /docker-entrypoint.sh
+
+if [ "$1" == "test" ]; then
+    export PYTHONPATH=$PYTHONPATH:/app/src
+    python3 -m unittest discover
+else
+    uwsgi --ini /app/uwsgi.ini &
+    source /docker-entrypoint.sh
+fi
