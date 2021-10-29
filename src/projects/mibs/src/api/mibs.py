d67c68779c270fa53597ad727f1b4fe636e53651
Joshua Spilchen <jds679@usask.ca>
2021-10-29 01:57:34 -0600

    Fixed issues with GET implementation, set up tests for re-implementation Closes #65

--- src/projects/mibs/src/api/mibs.py
+++ src/projects/mibs/src/api/mibs.py
@@ -2,19 +2,39 @@
 /mibs GET POST PUT DELETE endpoints
 '''
 
-from flask import Blueprint, json
-from ../models/__init__ import db
+import Any, Dict, List, Tuple, Union
+from flask import Blueprint, json, request
+from flask.helpers import url_for
+from dateutil.parser import parse as datetimeParse
+from http import HTTPStatus
+
+from lib.mibs.python.openapi.swagger_server.models import MessageInABottle, EmailRecipient
+from lib.mibs.python.openapi.swagger_server.models.any_of_message_in_a_bottle_recipients_items \
+    import AnyOfMessageInABottleRecipientsItems
+from lib.mibs.python.openapi.swagger_server.models.sms_recipient import SmsRecipient
+from lib.mibs.python.openapi.swagger_server.models.user_recipient import UserRecipient
+from models import Message, EmailMessageRecipient, db
 
 mibs_blueprint = Blueprint('mibs', __name__, url_prefix='/mibs')
 
 
 @mibs_blueprint.route('', methods=['GET'])
 def get():
-    id = request.args.get(messageId)
-    return {
-      'success': json.dumps(True),
-      'message': 'Hello from GET /mibs',
-    }
+    assert request is not None
+    id = request.args.get("messageId")
+
+    # if no message_id is given, retrieve all mibs for user
+    if id is None:
+        return json.dumps(Message.filter_by(user_id=1)), HTTPStatus.OK
+
+    # message_id is given
+    else:
+        mib = Message.filter_by(user_id=1, message_id=id)
+        # no mib matching id, return nothing
+        if mib is None:
+            return "Message not found", HTTPStatus.NOT_FOUND
+        else:
+            return json.dumps(mib), HTTPStatus.OK
 
 
 @mibs_blueprint.route('', methods=['POST'])
@@ -47,5 +67,4 @@ def delete():
     return {
       'success': json.dumps(True),
       'message': 'Hello from DELETE /mibs',
-    }
     }
--- src/projects/mibs/test/api/mibs_test.py
+++ src/projects/mibs/test/api/mibs_test.py
@@ -1,81 +1,123 @@
-from ../src/api/mibs import get
-
 """
-mibs-get
-Searching for MIB with a messageId that does not exist
-Expected outcome: all MIBS for the user are returned
+Stub for MIBS system
 """
+import unittest
+from flask import Flask, request
+from models import db, Message
+from api.mibs import mibs_blueprint
+from flask import HTTPStatus
+from datetime import datetime
+
+# mibs to populate the db
+filler_mibs = [
+    {"message_id": 1, "user_id": 1,
+        "message": "This was my first mibs message!",
+        "send_time": datetime.now()},
+    {"message_id": 2, "user_id": 1,
+        "message": "This was my second mibs message!",
+        "send_time": datetime.now()},
+    {"message_id": 3, "user_id": 2,
+        "message": "This is someone else's message!",
+        "send_time": datetime.now()},
+    {"message_id": 4, "user_id": 3,
+        "message": "There are more people making messages!",
+        "send_time": datetime.now()},
+    {"message_id": 5, "user_id": 1,
+        "message": "This is actually my third message (ignore the 5)!",
+        "send_time": datetime.now()},
+    {"message_id": 6, "user_id": 1,
+        "message": "I have a lot of messages!",
+        "send_time": datetime.now()},
+    {"message_id": 7, "user_id": 3,
+        "message": "Felt like I needed another message!",
+        "send_time": datetime.now()},
+    {"message_id": 8, "user_id": 2,
+        "message": "Me too!",
+        "send_time": datetime.now()},
+    {"message_id": 9, "user_id": 1,
+        "message": "We are almost up to ten mibs in the db!",
+        "send_time": datetime.now()}
+]
 
 
-def nonexistant_id_messages():
-    assert isinstance(json.loads(get(-1)).message, list)
+class TestMibsApi(unittest.TestCase):
+    '''
+    /mibs endpoint unit tests
+    '''
+
+    def setUp(self):
+        self.app = Flask(__name__)
+        self.app.config['TESTING'] = True
+        db.init_app(self.app)
+        with self.app.app_context():
+            db.create_all()
+        self.app.register_blueprint(mibs_blueprint)
+        self.client = self.app.test_client()
+
+    def tearDown(self):
+        with self.app.app_context():
+            db.session.remove()
+            db.drop_all()
 
 
-"""
-mibs-get
-Searching for MIB with a messageId that does not exist
-Expected outcome: a 404 response is returned
-"""
-
-
-def nonexistant_id_response():
-    assert json.loads(get(-1)).success == 404
-
-
-"""
-mibs-get
-Searching for a MIB with a valid messageId
-Expected outcome: the MIB with the corresponding messageId is returned
-"""
-
-
-def valid_id_message():
-    assert len(json.loads(get()).message) == 1
-
-
-"""
-mibs-get
-Searching for a MIB with a valid messageId that
-Expected outcome: a 200 response is returned
-"""
-
-
-def valid_id_response():
-    assert json.loads(get()).success == 200
-
-
-"""
-mibs-get
-Making request with no given messageId that
-Expected outcome: all MIBs for the user are returned
-"""
-
-
-def no_given_id_message():
+def test_no_mibs_exist():
+    """
+    mibs-GET
+    Trying to retireve mibs on an empty database
+    """
     pass
 
 
-"""
-mibs-get
-Making request with no given messageId
-Expected outcome: a 200 response is returned
-"""
+def test_request_with_nonexistant_id_():
+    """
+    mibs-GET
+    Searching for MIB with a messageId that does not exist
+    Expected outcome: all MIBS for the user are returned with a NOT_FOUND
+    response
+    """
+    populate_messages()
+    pass
 
 
-def no_given_id_response():
-    assert json.load(get()).response == 200
+def test_get_mib_with_valid_id():
+    """
+    mibs-GET
+    Searching for a MIB with a valid messageId
+    Expected outcome: the MIB with the corresponding messageId is returned with
+    an OKAY response
+    """
+    pass
 
 
-"""
-mibs-get
-Making a request from an unauthorized user
-Expected outcome: no Mibs are returned with a 401 response
-"""
+def test_no_given_message_id():
+    """
+    mibs-GET
+    Making request with no given messageId
+    Expected outcome: all MIBs for the user are returned with an OKAY response
+    """
+    pass
 
 
 def not_authorized():
-    assert json.loads(get()).response == 401
+    """
+    mibs-GET
+    Making a request from an unauthorized user
+    Expected outcome: no Mibs are returned with an UNAUTHORIZED response
+    """
+    pass
+
+
+def populate_messages():
+    """
+    Fills database with messages
+    """
+    for i in range(len(filler_mibs)):
+        db.session.add(Message(message_id=filler_mibs[i]["message_id"],
+                               user_id=filler_mibs[i]["user_id"],
+                               message=filler_mibs[i]["message"],
+                               send_time=filler_mibs[i]["send_time"]))
+    db.session.commit()
 
 
 if __name__ == '__main__':
-    print("*** Test Script Complete ***")
+    unittest.main()
