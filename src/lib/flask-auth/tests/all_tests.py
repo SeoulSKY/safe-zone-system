67e0f6ded9517b41ee18d440930a426221ec39f8
Zachary Ross <zmr462@usask.ca>
2021-11-08 11:06:40 -0600

    fix: remove unnecessary file, all_tests.py

--- src/lib/flask-auth/tests/all_tests.py
+++
@@ -1,7 +0,0 @@
-import unittest
-from utils_test import TestGetAccessToken
-from authenticator_test import TestAuthenticatorInit, TestAuthenticator
-
-
-if __name__ == '__main__':
-    unittest.main()
