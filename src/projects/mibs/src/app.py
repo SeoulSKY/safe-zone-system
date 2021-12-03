c60c115162ec672e11099f6ce6601cd12d5567ba
Joshua Spilchen <jds679@usask.ca>
2021-12-02 21:58:18 -0600

    More changes for checks Closes #200

--- src/projects/mibs/src/app.py
+++ src/projects/mibs/src/app.py
@@ -4,7 +4,6 @@ Stub for MIBS system
 from flask import Flask, request
 from os import environ as env
 from models import db, Message
-from auth import Authenticator, auth_token
 from src.api.mibs import mibs_blueprint
 from auth_init import auth
 
