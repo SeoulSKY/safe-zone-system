5a14ae2d431c7aa0ec32fa4fa530147d71b08be9
Emeka <cvu302>
2021-12-06 23:11:05 -0600

    chore

--- src/projects/mibs/test/services/test_email_service.py
+++ src/projects/mibs/test/services/test_email_service.py
@@ -1,18 +1,22 @@
+'''
+    Mibs polling Service unittest
+'''
 import unittest
-
-from api.mibs import mibs_blueprint, delete_mibs_for_user
+from api.mibs import mibs_blueprint
 from models import Message, EmailMessageRecipient, db
-from lib.mibs.python.openapi.swagger_server.models import MessageInABottle, EmailRecipient
+from lib.logger.safezone_logger import get_logger
 from flask import Flask
 from services.message_pool_service import MessagePoolingService
 from datetime import timedelta, datetime
-from lib.logger.safezone_logger import get_logger
 
 LOGGER = get_logger(__name__)
 TEMP_USER_ID='temp-user-id'
 TEST_EMAIL_DOMAIN = '.message_pool_service@email.com'
-class TestEmailService(unittest.TestCase):
 
+class TestEmailService(unittest.TestCase):
+    '''
+    Mibs polling Service unittest
+    '''
     def setUp(self):
         self.app = Flask(__name__)
         self.app.config['TESTING'] = True
@@ -24,17 +28,16 @@ class TestEmailService(unittest.TestCase):
         self.client = self.app.test_client()
         self.mibs = []
         self.last_week = (datetime.utcnow() - timedelta(weeks=1))
-        LOGGER.debug(self.mibs)
         self.add_mibs()
 
     def add_mibs(self):
         with self.app.app_context():
             for i in range(1, 5):
                 email_recipients = [EmailMessageRecipient(
-                    email=f"recipient{j}.testuser{i}{TEST_EMAIL_DOMAIN}") for j in range(3)]
+                    email=f'recipient{j}.testuser{i}{TEST_EMAIL_DOMAIN}') for j in range(3)]
                 message = Message(
                     user_id=TEMP_USER_ID,
-                    message=f"Test message for user{i}",
+                    message=f'Test message for user{i}',
                     send_time=self.last_week,
                     email_recipients=email_recipients)
                 self.mibs.append(message)
@@ -50,11 +53,12 @@ class TestEmailService(unittest.TestCase):
         with self.app.app_context():
             email_recipients_preupdate = EmailMessageRecipient.query.all()
             message_pool_service = MessagePoolingService()
+            # pylint: disable=W0212
             message_pool_service._update_recipients_send_attempt_time(email_recipients_preupdate)
-
             email_recipients_postupdate =  EmailMessageRecipient.query.all()
             for recipient in email_recipients_postupdate:
                 self.assertNotEqual(recipient.send_attempt_time, self.last_week)
+                # pylint: disable=W0212
                 self.assertEqual(recipient.send_attempt_time, message_pool_service._current_time)
 
 if __name__ == '__main__':
